eea9b3e87731917eb6017ecdd749c285f5aedadf
Lenoch <81lennoch@gmail.com>
2024-11-05 21:49:18 +0100

    Add port to settings

--- run.py
+++ run.py
@@ -21,10 +21,14 @@ def home():
 if __name__ == '__main__':
     print('Flask app started!')
     if config['development']['production']:
-        serve(app, host='0.0.0.0', port=5000)
+        serve(
+            app,
+            host='0.0.0.0',
+            port=config['development']['port']
+        )
     else:
         app.run(
             debug=True,
             host='0.0.0.0',
-            port=5000
+            port=config['development']['port']
         )
