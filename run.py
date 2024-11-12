a888d3cb1ebff560b9eb4558c8da3e7639e140ba
Lenoch <81lennoch@gmail.com>
2024-11-13 00:28:04 +0100

    Add game download info page
    Introduced a new route to display download statistics for a specific game by its TID.

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from utils.get_config import config
 from utils.render import start_periodic_render
-from flask import Flask
+from flask import Flask, render_template
 import os
+import json
 from waitress import serve
 
 print('Starting Flask app...')
@@ -16,6 +17,23 @@ start_periodic_render(app, output_path, config['interval_hours'])
 def home():
     with open('temp/index.html', 'r', encoding='utf-8') as file:
         return file.read()
+    
+@app.route('/<tid>')
+def game_info(tid: str):
+    tid = tid.upper()
+    
+    path = os.path.join(config['data_file_path'], f'{tid}_downloads.json')
+    if not os.path.isfile(path):
+        return 'TID not found', 404
+    
+    with open(path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        
+        return render_template(
+            'game_info.jinja',
+            tid=tid,
+            downloads=data,
+        )
 
 
 if __name__ == '__main__':
